37332fb14dda57b791b03fb7a865df26d35f10b7
Adriano Jucan <98047579+AdrianoJuc13@users.noreply.github.com>
2024-07-24 15:29:28 +0300

    routing links

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="/portofolio">
+      <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
