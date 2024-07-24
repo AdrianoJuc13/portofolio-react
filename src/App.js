12ed1fb0248c5535b692a2ead5e362b30d418005
Adriano Jucan <98047579+AdrianoJuc13@users.noreply.github.com>
2024-07-24 12:18:09 +0300

    hello

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css"; // Ensure this path is correct
+
+function App() {
+  return (
+    <div className="App">
+      My name is Adriano Jucan. This is my personal portofolio.
+    </div>
+  );
+}
+
+export default App;
