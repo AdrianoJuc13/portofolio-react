f74c29ccb8fc5359a4956944dcd725c6b1e648bd
Adriano Jucan <98047579+AdrianoJuc13@users.noreply.github.com>
2024-07-27 03:00:20 +0300

    text in the about section

--- src/components/About.js
+++ src/components/About.js
@@ -31,6 +31,141 @@ const About = () => {
         my passion for robotics with my technical skills to solve real-world
         problems and drive innovation.
       </p>
+      <h2>About Me</h2>
+      <p>
+        I am a 24-year-old robotics enthusiast, driven by a lifelong dream of
+        creating humanoid robots and enhancing human capabilities through
+        technology. Inspired by Iron Man, I devoted my high school years to
+        intensive studies in informatics and electronics, channeling my
+        curiosity and motivation into building various projects such as robotic
+        arms, boats, cars, airplanes, and gadgets of all type. My dedication led
+        me to participate in the national stage of the FIRST Tech Challenge in
+        Bucharest two times, earning the prize for Best Engineers.
+      </p>
+      <p>
+        After high school, I pursued my passion at the University of Essex in
+        the United Kingdom, where I delved into artificial intelligence,
+        electronics, genetic programming, control theory, and more. Alongside my
+        studies, I worked as a front-end freelancer on diverse projects
+        including mobile apps, CRM and ERP systems, risk management application,
+        web scraping, brochures, catalogues, 3D modeling, marketing, and event
+        organization.
+      </p>
+      <p>
+        I graduated as a Robotic Engineer, successfully completing a capstone
+        project that involved developing a data glove to control a 3D rigged
+        hand model in Unity. Now, I am eager to find a role where I can combine
+        my passion for robotics with my technical skills to solve real-world
+        problems and drive innovation.
+      </p>
+      <h2>About Me</h2>
+      <p>
+        I am a 24-year-old robotics enthusiast, driven by a lifelong dream of
+        creating humanoid robots and enhancing human capabilities through
+        technology. Inspired by Iron Man, I devoted my high school years to
+        intensive studies in informatics and electronics, channeling my
+        curiosity and motivation into building various projects such as robotic
+        arms, boats, cars, airplanes, and gadgets of all type. My dedication led
+        me to participate in the national stage of the FIRST Tech Challenge in
+        Bucharest two times, earning the prize for Best Engineers.
+      </p>
+      <p>
+        After high school, I pursued my passion at the University of Essex in
+        the United Kingdom, where I delved into artificial intelligence,
+        electronics, genetic programming, control theory, and more. Alongside my
+        studies, I worked as a front-end freelancer on diverse projects
+        including mobile apps, CRM and ERP systems, risk management application,
+        web scraping, brochures, catalogues, 3D modeling, marketing, and event
+        organization.
+      </p>
+      <p>
+        I graduated as a Robotic Engineer, successfully completing a capstone
+        project that involved developing a data glove to control a 3D rigged
+        hand model in Unity. Now, I am eager to find a role where I can combine
+        my passion for robotics with my technical skills to solve real-world
+        problems and drive innovation.
+      </p>
+      <h2>About Me</h2>
+      <p>
+        I am a 24-year-old robotics enthusiast, driven by a lifelong dream of
+        creating humanoid robots and enhancing human capabilities through
+        technology. Inspired by Iron Man, I devoted my high school years to
+        intensive studies in informatics and electronics, channeling my
+        curiosity and motivation into building various projects such as robotic
+        arms, boats, cars, airplanes, and gadgets of all type. My dedication led
+        me to participate in the national stage of the FIRST Tech Challenge in
+        Bucharest two times, earning the prize for Best Engineers.
+      </p>
+      <p>
+        After high school, I pursued my passion at the University of Essex in
+        the United Kingdom, where I delved into artificial intelligence,
+        electronics, genetic programming, control theory, and more. Alongside my
+        studies, I worked as a front-end freelancer on diverse projects
+        including mobile apps, CRM and ERP systems, risk management application,
+        web scraping, brochures, catalogues, 3D modeling, marketing, and event
+        organization.
+      </p>
+      <p>
+        I graduated as a Robotic Engineer, successfully completing a capstone
+        project that involved developing a data glove to control a 3D rigged
+        hand model in Unity. Now, I am eager to find a role where I can combine
+        my passion for robotics with my technical skills to solve real-world
+        problems and drive innovation.
+      </p>
+      <h2>About Me</h2>
+      <p>
+        I am a 24-year-old robotics enthusiast, driven by a lifelong dream of
+        creating humanoid robots and enhancing human capabilities through
+        technology. Inspired by Iron Man, I devoted my high school years to
+        intensive studies in informatics and electronics, channeling my
+        curiosity and motivation into building various projects such as robotic
+        arms, boats, cars, airplanes, and gadgets of all type. My dedication led
+        me to participate in the national stage of the FIRST Tech Challenge in
+        Bucharest two times, earning the prize for Best Engineers.
+      </p>
+      <p>
+        After high school, I pursued my passion at the University of Essex in
+        the United Kingdom, where I delved into artificial intelligence,
+        electronics, genetic programming, control theory, and more. Alongside my
+        studies, I worked as a front-end freelancer on diverse projects
+        including mobile apps, CRM and ERP systems, risk management application,
+        web scraping, brochures, catalogues, 3D modeling, marketing, and event
+        organization.
+      </p>
+      <p>
+        I graduated as a Robotic Engineer, successfully completing a capstone
+        project that involved developing a data glove to control a 3D rigged
+        hand model in Unity. Now, I am eager to find a role where I can combine
+        my passion for robotics with my technical skills to solve real-world
+        problems and drive innovation.
+      </p>
+      <h2>About Me</h2>
+      <p>
+        I am a 24-year-old robotics enthusiast, driven by a lifelong dream of
+        creating humanoid robots and enhancing human capabilities through
+        technology. Inspired by Iron Man, I devoted my high school years to
+        intensive studies in informatics and electronics, channeling my
+        curiosity and motivation into building various projects such as robotic
+        arms, boats, cars, airplanes, and gadgets of all type. My dedication led
+        me to participate in the national stage of the FIRST Tech Challenge in
+        Bucharest two times, earning the prize for Best Engineers.
+      </p>
+      <p>
+        After high school, I pursued my passion at the University of Essex in
+        the United Kingdom, where I delved into artificial intelligence,
+        electronics, genetic programming, control theory, and more. Alongside my
+        studies, I worked as a front-end freelancer on diverse projects
+        including mobile apps, CRM and ERP systems, risk management application,
+        web scraping, brochures, catalogues, 3D modeling, marketing, and event
+        organization.
+      </p>
+      <p>
+        I graduated as a Robotic Engineer, successfully completing a capstone
+        project that involved developing a data glove to control a 3D rigged
+        hand model in Unity. Now, I am eager to find a role where I can combine
+        my passion for robotics with my technical skills to solve real-world
+        problems and drive innovation.
+      </p>
     </div>
   );
 };
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
-  return <div className="Contact">Contact</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+    surname: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission, e.g., send data to an API or email service
+    alert(formData.surname);
+    console.log("Form data submitted:", formData);
+  };
+
+  return (
+    <div className="contact-section">
+      <h2>Contact Me</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+          <label htmlFor="surname">Surname</label>
+          <input
+            type="text"
+            id="surname"
+            name="surname"
+            value={formData.surname}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit" className="submit-button">
+          Send
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
